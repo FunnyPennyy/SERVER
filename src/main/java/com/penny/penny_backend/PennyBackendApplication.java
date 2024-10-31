9938db9ab368afa7a898821921d244df940ff44b
yuyi5187 <kimyy200063@gmail.com>
2024-11-01 00:06:23 +0900

    Project Setting

---
+++ src/main/java/com/penny/penny_backend/PennyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.penny.penny_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennyBackendApplication.class, args);
+	}
+
+}
