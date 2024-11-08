7f539e0d1e865db1cbcd753da37d319f35f3dec3
handa-777 <61150335+handa-777@users.noreply.github.com>
2024-11-09 02:10:01 +0900

    Delete src/main/java/com/penny/penny_backend/account directory

--- src/main/java/com/penny/penny_backend/account/repository/TeacherAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.penny.penny_backend.account.repository;
-
-import com.penny.penny_backend.account.entity.TeacherAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeacherAccountRepository extends JpaRepository<TeacherAccount, Long> {
-}
