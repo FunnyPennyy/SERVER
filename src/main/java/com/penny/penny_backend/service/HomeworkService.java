aba666afb40b8132aa8027584bd5a941be12acc5
handa <handa0704@naver.com>
2024-11-28 15:36:47 +0900

    homework 관련 파일 주석처리

--- src/main/java/com/penny/penny_backend/service/HomeworkService.java
+++ src/main/java/com/penny/penny_backend/service/HomeworkService.java
@@ -6,9 +6,8 @@ import java.util.List;
 
 public interface HomeworkService {
     // 학급에 있는 학생들의 숙제 상태 조회
-    List<HomeworkStatus> getHomeworkStatusByClassId(Long classId);
-
-    // 숙제 상태 수정
-    void updateHomeworkStatusByClassId(Long classId, Long homeworkId, boolean status);
-}
+//    List<HomeworkStatus> getHomeworkStatusByClassId(Long classId);
 //
+//    // 숙제 상태 수정
+//    void updateHomeworkStatusByClassId(Long classId, Long homeworkId, boolean status);
+}
--- src/main/java/com/penny/penny_backend/service/HomeworkServiceImpl.java
+++ src/main/java/com/penny/penny_backend/service/HomeworkServiceImpl.java
@@ -7,31 +7,31 @@ import com.penny.penny_backend.repository.StudentRepository;
 import java.util.List;
 
 public class HomeworkServiceImpl implements HomeworkService {
-    private final HomeworkStatusRepository homeworkStatusRepository;
-    private final StudentRepository studentRepository;
-
-    public HomeworkServiceImpl (HomeworkStatusRepository homeworkStatusRepository, StudentRepository studentRepository) {
-        this.homeworkStatusRepository = homeworkStatusRepository;
-        this.studentRepository = studentRepository;
-    }
+//    private final HomeworkStatusRepository homeworkStatusRepository;
+//    private final StudentRepository studentRepository;
 //
-    @Override
-    public List<HomeworkStatus> getHomeworkStatusByClassId(Long classId) {
-        // 학급 ID로 학생 ID들을 조회한 후, 해당 학생들의 숙제 상태 조회
-        List<Long> studentIds = studentRepository.findStudentIdsByClassId(classId);
-        return homeworkStatusRepository.findByStudentIdIn(studentIds);
-    }
-
-    @Override
-    public void updateHomeworkStatusByClassId(Long classId, Long homeworkId, boolean status) {
-        // 학급 ID로 학생 ID들을 조회한 후, 해당 학생들의 숙제 상태를 수정
-        List<Long> studentIds = studentRepository.findStudentIdsByClassId(classId);
-        List<HomeworkStatus> homeworkStatuses = homeworkStatusRepository.findByStudentIdInAndHomeworkId(studentIds, homeworkId);
-
-        for (HomeworkStatus homeworkStatus : homeworkStatuses) {
-            homeworkStatus.setStatus(status);
-        }
-
-        homeworkStatusRepository.saveAll(homeworkStatuses);
-    }
+//    public HomeworkServiceImpl (HomeworkStatusRepository homeworkStatusRepository, StudentRepository studentRepository) {
+//        this.homeworkStatusRepository = homeworkStatusRepository;
+//        this.studentRepository = studentRepository;
+//    }
+////
+//    @Override
+//    public List<HomeworkStatus> getHomeworkStatusByClassId(Long classId) {
+//        // 학급 ID로 학생 ID들을 조회한 후, 해당 학생들의 숙제 상태 조회
+//        List<Long> studentIds = studentRepository.findStudentIdsByClassId(classId);
+//        return homeworkStatusRepository.findByStudentIdIn(studentIds);
+//    }
+//
+//    @Override
+//    public void updateHomeworkStatusByClassId(Long classId, Long homeworkId, boolean status) {
+//        // 학급 ID로 학생 ID들을 조회한 후, 해당 학생들의 숙제 상태를 수정
+//        List<Long> studentIds = studentRepository.findStudentIdsByClassId(classId);
+//        List<HomeworkStatus> homeworkStatuses = homeworkStatusRepository.findByStudentIdInAndHomeworkId(studentIds, homeworkId);
+//
+//        for (HomeworkStatus homeworkStatus : homeworkStatuses) {
+//            homeworkStatus.setStatus(status);
+//        }
+//
+//        homeworkStatusRepository.saveAll(homeworkStatuses);
+//    }
 }
