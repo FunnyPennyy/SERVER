2cb428e1508364075683119f841adc06acdf188a
yuyi5187 <kimyy200063@gmail.com>
2024-11-30 20:38:24 +0900

    [fix] login 구현

--- src/main/java/com/penny/penny_backend/config/SecurityConfig.java
+++ src/main/java/com/penny/penny_backend/config/SecurityConfig.java
@@ -2,21 +2,25 @@ package com.penny.penny_backend.config;
 
 import com.penny.penny_backend.jwt.JwtAuthenticationFilter;
 import com.penny.penny_backend.jwt.JwtTokenProvider;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+
 
 import java.security.cert.CertPathBuilder;
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -25,8 +29,36 @@ public class SecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+
+        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource(){
+            @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(Collections.singletonList("http://localhost:3000/"));
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.setAllowCredentials(true);
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setMaxAge(3600L); //1시간
+                return config;
+            }
+        }));
+
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/members/sign-in").permitAll()
+                        .requestMatchers("/members/sign-up").permitAll()
+                        .anyRequest().authenticated()
+
+                )
+                .formLogin((form)-> form.disable())
+                .logout((logout)-> logout.permitAll());
+/*        http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(Customizer.withDefaults())
 //                .csrf().disable()
@@ -40,14 +72,12 @@ public class SecurityConfig {
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }*/
+
+        http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        // BCrypt Encoder 사용
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
 }
 
 
