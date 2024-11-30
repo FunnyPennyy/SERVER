4f7508c3ee12f04ab8975d11341d91ed5d16a472
yuyi5187 <kimyy200063@gmail.com>
2024-11-30 20:44:59 +0900

    [fix] 모든 api 권한 허용

--- src/main/java/com/penny/penny_backend/config/SecurityConfig.java
+++ src/main/java/com/penny/penny_backend/config/SecurityConfig.java
@@ -53,7 +53,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/members/sign-in").permitAll()
                         .requestMatchers("/members/sign-up").permitAll()
-                        .anyRequest().authenticated()
+                        //.anyRequest().authenticated() //그 외는 권한 필요
+                        .anyRequest().permitAll()
 
                 )
                 .formLogin((form)-> form.disable())
